dec70ca8ffd1cfc58af5b348767d319fd7a308c8
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-03-31 08:51:26 +0900

    remove @next/mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
-import withMDX from "@next/mdx";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   output: "export",
-  images: { unoptimized: true },
 };
 
-export default withMDX()(nextConfig);
+export default nextConfig;
