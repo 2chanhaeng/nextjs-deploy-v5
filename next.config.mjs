95d91df38ce7ed6898f93ddea7b4465a60e350a1
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-03-31 09:04:16 +0900

    import @next/mdx and not use
    This reverts commit dec70ca8ffd1cfc58af5b348767d319fd7a308c8.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+import withMDX from "@next/mdx";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   output: "export",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
